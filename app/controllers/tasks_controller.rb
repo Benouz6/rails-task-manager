a6edf12cc8b7c628a3852de4272ea6f3c81b14e0
benoit leboucher <ben.lebou@gmail.com>
2021-11-09 14:55:51 -0500

    Update is working now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,31 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
   def new
+    @task = Task.new
+  end
 
+  # post '/tasks', to: 'tasks#create', as: :tasks
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to :tasks
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to :tasks
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # Create a task
   get '/tasks/new', to: 'tasks#new', as: :new_task
-  post "/tasks", to: "tasks#create", as: :tasks
+  post '/tasks', to: 'tasks#create', as: :tasks
 
   # Read a task
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
 end
